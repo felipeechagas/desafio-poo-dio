478edd7d64e76979996bfc38a37d093595d9612d
Felipe Chagas <felipechagasferreira@gmail.com>
2023-01-02 15:50:51 -0300

    Final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -20,8 +20,40 @@ public class Main {
         mentoria1.setDescricao("Mentoria Básico para Iniciantes");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devFelipe = new Dev();
+        devFelipe.setNome("Felipe");
+        devFelipe.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Felipe: " + devFelipe.getConteudosInscritos());
+        devFelipe.progredir();
+        devFelipe.progredir();
+        devFelipe.progredir();
+        devFelipe.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos inscritos Felipe: " + devFelipe.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Felipe: " + devFelipe.getConteudosConcluidos());
+        System.out.println("Xp: " + devFelipe.calcularTotalXp());
+
+        System.out.println("---------------");
+
+        Dev devLeticia = new Dev();
+        devLeticia.setNome("Leticia");
+        devLeticia.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Leticia" + devLeticia.getConteudosInscritos());
+        devLeticia.progredir();
+        devLeticia.progredir();
+        System.out.println("Conteúdos concluídos Leticia" + devLeticia.getConteudosConcluidos());
+        System.out.println("Xp: " + devLeticia.calcularTotalXp());
+
     }
 }
